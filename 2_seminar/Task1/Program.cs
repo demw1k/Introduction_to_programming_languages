ada841b9b56325b906655f658e614ca73bf8a344
demw1k <Zotov.yura1991@yandex.ru>
2024-10-13 14:16:49 +0400

    solving the first task 2 seminar

---
+++ 2_seminar/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задание 1. Совместная работа
+// Семинар 2. Простые алгоритмы на C#
+// Напишите программу, которая принимает на
+// вход трёхзначное число и удаляет вторую
+// цифру этого числа.
+
+Console.WriteLine("Введите целое положительное ТРЁХЗНАЧНОЕ число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+
+    int result = firstDigit * 10 + thirdDigit;
+
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Некорректный ввод.");
+}
