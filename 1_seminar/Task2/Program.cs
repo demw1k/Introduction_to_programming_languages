934e4fa4075de6629c3a8ca7ebf9f511e534c62c
demw1k <Zotov.yura1991@yandex.ru>
2024-10-13 07:59:11 +0400

    Added task solution

--- 1_seminar/Task2/Program.cs
+++ 1_seminar/Task2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2
+
+Console.WriteLine("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 0) // Проверка на положительное число
+{
+    int count = -num; // num * -1
+
+    while (count <= num)
+    {
+        Console.Write(count + " ");
+        count++; // count = count + 1;
+                 // count += 1;
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода!");
+}
+
+
+
