1d8fe006aa4f31825bcca37c3f0e788226e0a685
demw1k <Zotov.yura1991@yandex.ru>
2024-10-13 11:06:13 +0400

    solving the 4 task

---
+++ HW_1_seminar/Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вывести нечетные числа Описание: Напишите метод, который на вход принимает число (number), 
+// а на выходе выводит все нечетные числа от 1 до number (включительно), после каждого числа должен быть знак пробела.
+
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= num)
+{
+    Console.Write(count + " ");
+    count = count + 2;
+}
+
