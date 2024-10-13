9952bfe43dc3556099032a8bf7426a1b6ef4460d
demw1k <Zotov.yura1991@yandex.ru>
2024-10-13 15:09:40 +0400

    solving the 2 task 2 seminar

---
+++ 2_seminar/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задание 2. Работа в сессионных залах
+// Семинар 2. Простые алгоритмы на C#
+// Напишите программу, которая принимает на вход
+// трёхзначное число и возводит вторую цифру этого
+// числа в степень, равную третьей цифре.
+
+using System.Reflection;
+
+Console.WriteLine("Введите целое положительное ТРЁХЗНАЧНОЕ число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    int secondDigit = num / 10 % 10;
+    int thirdDigit = num % 10;
+
+    int result = secondDigit;
+
+    for (int i = 1; i < thirdDigit; i++)
+    {
+        result = result * secondDigit; // result *= secondDigit
+    }
+    Console.WriteLine(result);
+}
+else Console.WriteLine("Некорректный ввод.");
+
+// ИЛИ ЧЕРЕЗ ЦИКЛ WHILE
+
+// int count = 1; // вводим счетчик
+
+// while (count < thirdDigit)
+// {
+//     result = result * secondDigit;
+//     count++;
+// }
