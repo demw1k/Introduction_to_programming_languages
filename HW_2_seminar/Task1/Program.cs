39f7bd22df350994095329551c00fcc152567358
demw1k <Zotov.yura1991@yandex.ru>
2024-10-13 18:41:38 +0400

    solving the 1 task 2 HW seminar

---
+++ HW_2_seminar/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли оно
+// одновременно 7 и 23
+
+// a=50 => нет
+// a=7 => нет
+// a=322 => да
+
+
+
+Console.WriteLine("Input your number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 7 == 0 && num % 23 == 0)
+    {
+        Console.WriteLine("Yes");
+    }
+else
+{
+    Console.WriteLine("No");
+}
