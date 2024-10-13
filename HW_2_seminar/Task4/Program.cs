c66ea4db26ee1767342696a4c92c4729bb11c6ae
demw1k <Zotov.yura1991@yandex.ru>
2024-10-13 20:34:43 +0400

    adjust the process 3 task 2 HW seminar

---
+++ HW_2_seminar/Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 4: Напишите программу, которая на вход
+// принимает натуральное число N, а на выходе
+// показывает его цифры через запятую.
+// 568 => 5,6,8
+// 8 => 8
+// 9542 => 9,5,4,2
+
+Console.WriteLine("Enter your number");
+int enterNumber = Convert.ToInt32(Console.ReadLine());
+
+if (enterNumber < 10)
+{
+    Console.WriteLine(enterNumber);
+}
+else
+{
+    while (enterNumber > 0)
+    {
+        int remainder = enterNumber % 10;
+        enterNumber = enterNumber / 10;
+        if (enterNumber > 0)
+        {
+            Console.Write(remainder + ",");
+        }
+        else
+        {
+            Console.Write(remainder);
+        }
+    }
+}
