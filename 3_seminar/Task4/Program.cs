dc4608cd03c531e1f9990c5c698e0ea1e95ad9b9
demw1k <Zotov.yura1991@yandex.ru>
2024-10-14 23:45:30 +0400

    add Seminar3:Task4

---
+++ 3_seminar/Task4/Program.cs
@@ -0,0 +1,43 @@
+﻿// Дано натуральное трёхзначное число. Создайте массив, состоящий из
+// цифр этого числа. Младший разряд числа должен располагаться на 0-
+// м индексе массива, старший – на 2-м. 
+// Пример
+// 456 => [6 5 4]
+// 781 => [1 8 7]
+
+
+
+
+
+
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int numCopy =num;
+int count = 0;
+
+//Считаем кол-во цифр в числе
+
+while (numCopy != 0)
+{
+    numCopy /= 10;
+    count ++;
+}
+
+// Cоздаем массив по кол-ву цифр
+
+int [] arr = new int [count];
+int index = 0;
+while (num != 0)
+{
+    arr[index] = num % 10;
+    num /= 10;
+    index++;
+}
+Console.Write("[");
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"{arr[i]} ");
+}
+Console.Write("]");
