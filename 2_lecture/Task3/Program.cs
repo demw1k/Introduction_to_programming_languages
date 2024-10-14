0b3ee37cf843d3f6fd56fba7f38ad3560f43d286
demw1k <Zotov.yura1991@yandex.ru>
2024-10-14 12:14:33 +0400

    add lecture2:Task3

---
+++ 2_lecture/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+int n = 10;
+int[] array = { 2, 5, 4, 7, 8, 3, 5, 1, 3, 8 };
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] == 0)
+    {
+        Console.Write($"{array[i]} ");
+        
+    }
+    i = i + 1;
+}
