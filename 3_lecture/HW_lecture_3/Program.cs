a15c07785fb23d90e063a593eabf7ff523bf3b75
demw1k <Zotov.yura1991@yandex.ru>
2024-10-16 10:41:01 +0400

    add lecture3:HW1

---
+++ 3_lecture/HW_lecture_3/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = i + 1;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int GetSumofElements(int[] arr)
+{
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int sum = 0;
+        int sum = sum + arr[i];
+
+    }
+    return sum;
+}
+
+// int GetProductofElements(int[] arr)
+// {
+
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         product = 1;
+//         product = product * arr[i];
+//     }
+//     return product;
+// }
+
+
+int n = 10;
+int[] arr = new int[n];
+FillArray(arr);
+PrintArray(arr);
+int sum = GetSumofElements(arr);
+// int product = GetProductofElements(arr);
+Console.WriteLine(sum);
+// Console.WriteLine(product);
