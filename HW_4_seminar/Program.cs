b67bed006fa7a4a7ae4c7af03426585d5c48e0e1
demw1k <Zotov.yura1991@yandex.ru>
2024-10-17 18:55:33 +0400

    add HW_4_seminar:Task1

---
+++ HW_4_seminar/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1: 
+// Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого четная.
+
+// 5 12 16 q [STOP] 
+// 3 45 342 15 [STOP]
+
+
+while (true)
+{
+    Console.WriteLine("Введите целое число или 'q' для выхода: ");
+    string input = Console.ReadLine();
+
+    if (input == "q")
+    {
+        break;
+    }
+
+    int number;
+    if (int.TryParse(input, out number))
+    {
+        int sum = 0;
+        while (number > 0)
+        {
+            sum = sum + (number % 10);
+            number = number / 10;
+        }
+        if (sum % 2 == 0)
+        {
+            Console.WriteLine("STOP");
+            break;
+        }
+    }
+
+
+}
