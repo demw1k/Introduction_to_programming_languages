c02b43087bf503c258b98dcfe399090a346e4ab4
demw1k <Zotov.yura1991@yandex.ru>
2024-10-18 19:40:07 +0400

    add 4_lecture:Task3

---
+++ 4_lecture/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Считать с консоли строку, состоящую из
+// цифр и латинских букв.
+// Сформировать новую строку, состоящую из
+// букв исходной строки.
+
+
+
+string GetLetters(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if( char.IsAsciiLetter(e) == true)
+       {
+            letters = letters + e;
+        }
+
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLetters(str);
+Console.WriteLine(result);
