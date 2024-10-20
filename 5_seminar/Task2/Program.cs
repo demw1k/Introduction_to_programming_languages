12b749dcea3da252c63416bdbf14e890d0a4c631
demw1k <Zotov.yura1991@yandex.ru>
2024-10-20 14:15:07 +0400

    add 5_semiar: Task2

---
+++ 5_seminar/Task2/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и
+// т.д.
+// Пример
+// 2 3 4 3
+//  4 3 4 1 => 2 + 3 + 5 = 10
+//  2 9 5 4
+
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random(); 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+        
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+   
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+         Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            Console.Write($"{matrix [i,j], 4} "); // , 4 это резерв длины для вывода значения
+        }
+        Console.WriteLine("|");
+    }
+    
+}
+
+
+int SumMainDiagonal(int[,] matrix)
+{
+    int sum = 0;
+
+    // int minSize = matrix.GetLength(0);
+    // if (matrix.GetLength(1)< minSize)
+    // {
+    //     minSize = matrix.GetLength(1);
+    // }
+
+    for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1); i++)
+    {
+        sum += matrix[i,i]; 
+    }
+    return sum;
+}
+
+int[,] array2d = CreateMatrixRndInt(5,4,1,10);
+PrintMatrix(array2d);
+
+Console.WriteLine();
+
+int result = SumMainDiagonal(array2d);
+Console.Write(result);
