d5fbe5a96634dbd3ff529534208e9ed8fc659f4e
demw1k <Zotov.yura1991@yandex.ru>
2024-10-22 18:26:24 +0400

    add hw_6_seminar: Task2

---
+++ HW_6_seminar/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные
+
+// “aBcD1ef!-” => “abcd1ef!-”
+
+
+Console.WriteLine("Задайте строку, содержащую латинские буквы в обоих регистрах.");
+string str = Console.ReadLine();
+string strResult = string.Empty;
+
+for (int i = 0; i < str.Length; i++)
+{
+    if (char.IsUpper(str[i]))
+    {
+        strResult += char.ToLower(str[i]);
+    }
+    else
+    {
+        strResult += str[i];
+    }
+}
+
+Console.WriteLine($" '{str}' => '{strResult}'");
