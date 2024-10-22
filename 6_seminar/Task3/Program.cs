57203340c20a7264d6db6936dbc70f0161e1d2ad
demw1k <Zotov.yura1991@yandex.ru>
2024-10-22 17:28:45 +0400

    add 6_seminar: Task3

--- 6_seminar/Task3/Program.cs
+++ 6_seminar/Task3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+// Пример
+// “hello” => 2
+// “world” => 1
+
+
+
+int CountVowels(string str)
+{
+    string vowels = "aeiouy";
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        for (int j = 0; j < vowels.Length; j++)
+        {
+            if (vowels[j] == str[i])
+            {
+                count++;
+            }
+        }
+        
+    }
+    return count;
+}
+
+
+Console.WriteLine("Введите строку: ");
+string start = Console.ReadLine();
+int result = CountVowels(start);
+Console.WriteLine($" '{start}' => {result}");
