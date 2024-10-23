e7d68aee2ca4c89c7a9d332d0596e879f72df58b
demw1k <Zotov.yura1991@yandex.ru>
2024-10-23 05:45:16 +0400

    add hw_6_seminar: Task3

--- HW_6_seminar/Task3/Program.cs
+++ HW_6_seminar/Task3/Program.cs
@@ -4,3 +4,21 @@
 // “aBcD1ef!-” => Нет
 // “шалаш” => Да
 // “55655” => Да
+
+
+
+Console.WriteLine("Введите строку, для проверки на полиндром: ");
+string polindrom = Console.ReadLine();
+for (int i = 0; i < polindrom.Length / 2; i++)
+{
+    if (polindrom[i] == polindrom[polindrom.Length -1 - i])
+    {
+        Console.WriteLine("Является полиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Не является полиндромом");
+        break;
+    }
+    Console.WriteLine("Является полиндромом");
+}
