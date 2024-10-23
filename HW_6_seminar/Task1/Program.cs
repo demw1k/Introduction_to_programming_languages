e78649243c4568f47b07f414c3641c1d81283d14
demw1k <Zotov.yura1991@yandex.ru>
2024-10-23 05:15:14 +0400

    add hw_6_seminar: Task1

--- HW_6_seminar/Task1/Program.cs
+++ HW_6_seminar/Task1/Program.cs
@@ -3,3 +3,39 @@
 
 // a b c => “abcdef”
 // d e f 
+
+
+
+void PrintMatrix(char[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} "); // , 4 это резерв длины для вывода значения
+        }
+        Console.WriteLine("|");
+    }
+
+}
+
+string RowIn2dArray(char[,] charArr)
+{
+    string res = "";
+    for (int i = 0; i < charArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < charArr.GetLength(1); j++)
+        {
+            res += charArr[i, j];
+        }
+
+    }
+    return res;
+}
+
+char[,] charArray = new char[,] { { 'a', 'b', 'c' }, { 'd', 'e', 'f' } };
+PrintMatrix(charArray);
+string result = RowIn2dArray(charArray);
+Console.WriteLine(result);
