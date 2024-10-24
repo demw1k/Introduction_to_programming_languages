1981f6927b56c988e6b16baff95f06346e038715
demw1k <Zotov.yura1991@yandex.ru>
2024-10-24 20:39:20 +0400

    add  7 seminar:Task1 and hw_7_seminar:Task1

---
+++ 7_seminar/task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// N=5 => 1 2 3 4 5
+
+
+
+void PrintNaturalNumbers(int num)
+{
+    if (num == 0) return;
+    PrintNaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+
+}
+
+Console.WriteLine(" Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+PrintNaturalNumbers(number);
+
+// Это пример ХВОСТОВОЙ рекурсии, т.к. самовызов находится в конце
+
+// void PrintNaturalNumbers(int num)
+// {
+//     if (num == 0) return;
+//     Console.Write($"{num} ");
+//     PrintNaturalNumbers(num - 1);
+
+// }
---
+++ HW_7_seminar/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+
+
+void PrintNaturalNumbers(int N, int M)
+{
+    if (N == M - 1) return;
+    PrintNaturalNumbers(N - 1, M);
+    Console.Write($"{N} ");
+}
+
+Console.WriteLine(" Введите натуральное число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите натуральное число M: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+PrintNaturalNumbers(number, number1);
