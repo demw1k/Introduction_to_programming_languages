1be3a452aa39a94988abc15aaf334cec57e92baf
demw1k <Zotov.yura1991@yandex.ru>
2024-10-25 08:51:54 +0400

    add hw_7_seminar:Task2 - fixed, working algorithm

--- HW_7_seminar/Task2/Program.cs
+++ HW_7_seminar/Task2/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 2: Напишите программу вычисления функции
 // Аккермана с помощью рекурсии. Даны два
 // неотрицательных числа m и n. 
-// m = 2, n = 3 -> A(m,n) = 29
+
 
 // Псевдокод
 
@@ -13,28 +13,28 @@
 //    еще
 //      вернуть ack(n - 1, ack (n, m - 1))
 
-// int Ackermann(int n, int m)
-// {
-//     if (n == 0)
-//     {
-//         return m + 1;
-//     }
-//     else if (m == 0)
-//     {
-//         return Ackermann(n - 1, 1);
-//     }
-//     else
-//     {
-//         return Ackermann(n - 1, Ackermann(n, m - 1));
-//     }
-// }
+int Ackermann(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    else if (m == 0)
+    {
+        return Ackermann(n - 1, 1);
+    }
+    else
+    {
+        return Ackermann(n - 1, Ackermann(n, m - 1));
+    }
+}
 
-// Console.Write("Введите неотрицательное число M: ");
-// int num1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите неотрицательное число N: ");
-// int num2 = Convert.ToInt32(Console.ReadLine());
-// int res = Ackermann(num1, num2);
-// Console.WriteLine($"{res}");
+Console.Write("Введите неотрицательное число M: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите неотрицательное число N: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int res = Ackermann(num1, num2);
+Console.WriteLine($"{res}");
 
 // Второй псевдокод
 
@@ -47,27 +47,27 @@
 //      n:= n - 1
 //    вернуть m + 1
 
-int Ackermann(int n, int m)
-{
-    while (n != 0)
-    {
-        if (m == 0)
-        {
-            m = 1;
-        }
-        else
-        {
-            m = Ackermann(n, m - 1);
+// int Ackermann(int n, int m)
+// {
+//     while (n != 0)
+//     {
+//         if (m == 0)
+//         {
+//             m = 1;
+//         }
+//         else
+//         {
+//             m = Ackermann(n, m - 1);
 
-        }
-        n = n - 1;
-    }
-    return m + 1;
-}
+//         }
+//         n = n - 1;
+//     }
+//     return m + 1;
+// }
 
-Console.Write("Введите неотрицательное число M: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите неотрицательное число N: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-int res = Ackermann(num1, num2);
-Console.WriteLine($"{res}");
+// Console.Write("Введите неотрицательное число M: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите неотрицательное число N: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int res = Ackermann(num1, num2);
+// Console.WriteLine($"{res}");
